f3016d5c4aee58952262fbce38de2979f226030c
Satya <100060852+CrypticFate5@users.noreply.github.com>
2022-08-21 09:05:15 +0530

    Update Problem 2: Finding Complexity using Counter Method Quiz.c

--- Finding Time Complexity of Algorithms/Problem 2: Finding Complexity using Counter Method Quiz.c
+++ Finding Time Complexity of Algorithms/Problem 2: Finding Complexity using Counter Method Quiz.c
@@ -1,3 +1,31 @@
+// Problem Statement 2:
+//  Convert the following algorithm into a program and find its time
+// complexity using the counter method.
+// void function (int n)
+// {
+//     int i= 1, s =1;
+//     while
+// (s <= n)
+//     {
+//                         i++;
+//                         s += i;
+//      }      
+// }     
+// Note: No need of counter increment for declarations and scanf() and  count variable printf() statements.
+// Manually find the complexity using counter method and write the
+// same in observation
+// Input:
+//  A positive Integer n
+// Output:
+// Print the value of the counter variable
+
+
+// For example:
+
+// Input	Result
+// 9
+// 12
+
 #include <stdio.h>
 void function(int n,int*c){
     int i=1,s=1;
